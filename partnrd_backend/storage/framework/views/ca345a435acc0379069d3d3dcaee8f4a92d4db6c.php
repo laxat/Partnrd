bbd4bfa9d1102d739c4ebe726e5796099083fa8e
laxat <fatman7100@gmail.com>
2022-05-10 22:53:52 -0400

    intial commit

---
+++ partnrd_backend/storage/framework/views/ca345a435acc0379069d3d3dcaee8f4a92d4db6c.php
@@ -0,0 +1,2 @@
+[<?php echo e($slot); ?>](<?php echo e($url); ?>)
+<?php /**PATH C:\xampp\htdocs\Partnr\partnrd\partnrd_backend\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
---
+++ partnrd_backend/storage/framework/views/e6ff0799fe907f650a0b2745ea3109114afd7d42.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH C:\xampp\htdocs\Partnr\partnrd\partnrd_backend\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/subcopy.blade.php ENDPATH**/ ?>
